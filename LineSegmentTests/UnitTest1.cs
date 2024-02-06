473b6430b62bbf0783b06bb7d8bbdcf5a58de0b8
Nuriye <nuriyeerdogan38@gmail.com>
2024-02-06 14:27:18 +0100

    feat: tests

--- LineSegmentTests/UnitTest1.cs
+++ LineSegmentTests/UnitTest1.cs
@@ -19,6 +19,20 @@ namespace LineSegmentTests
             Assert.AreEqual("2..7", result);
         }
 
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            // arrange
+            var LineSegment = new LineSegment(0, 5);
+            // act
+            int pointWithin = 3;
+            Assert.IsTrue(LineSegment.Contains(pointWithin), "Point should be within the line segment");
+
+            // assert.
+            int pointOutside = -1;
+            Assert.IsFalse(LineSegment.Contains(pointOutside), "Point should be outside the line segment");
+        }
   
 
     }
